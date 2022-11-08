f02af22a8743cd5148512281e1671e352c120283
jeremysuh02 <jeremysuh@utexas.edu>
2022-11-07 19:40:12 -0600

    scirpt change

--- LunarFlash/Assets/Scripts/ReeceScripts/randomSpawn.cs
+++ LunarFlash/Assets/Scripts/ReeceScripts/randomSpawn.cs
@@ -16,7 +16,7 @@ public class randomSpawn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        randomSpawn
+        randomSpawn();
         //Instantiate enemies?
     }
 
