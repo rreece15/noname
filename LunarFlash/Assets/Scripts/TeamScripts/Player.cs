e0173f796cdef218ef00a0febbf4ce4d06d78e3f
Boram Lee <boram_lee@utexas.edu>
2022-11-30 20:24:00 -0600

    player initial pos

--- LunarFlash/Assets/Scripts/TeamScripts/Player.cs
+++ LunarFlash/Assets/Scripts/TeamScripts/Player.cs
@@ -57,7 +57,7 @@ public class Player : MonoBehaviour
         defaultMovingSpeed = movingSpeed;
         isGameOver = false;
         isGameClear = false;
-        this.gameObject.transform.position = new Vector3(380f, 6, 586);
+        this.gameObject.transform.position = new Vector3(380f, 10, 586);
        
         playerController = this.gameObject.GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
